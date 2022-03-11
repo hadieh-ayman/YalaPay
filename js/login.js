d4d4f91249c2bf2be66ae52ce8496b942a5a0d63
Hadieh Abdulmajeed <75228467+hadieh-ha1905768@users.noreply.github.com>
2022-03-11 21:18:05 +0300

    login updated

--- js/login.js
+++ js/login.js
@@ -2,51 +2,27 @@ import { UserRepo } from "./repository/userRepo.js";
 
 const userRepo = new UserRepo();
 
-fetch("../YalaPay-data/users.json")
-    .then(function(resp){
-        return resp.json();
-    }).then(function(data){
-        console.log(data);
-})
-
-const submitBtn = document.querySelector(".submit-btn");
 const loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", validate);
 
 function formToObject(form) {
-    const formdata = new FormData(form);
-    const data = {};
-    for (const [key, value] of formdata) {
-      data[key] = value;
+    const forminfo = new FormData(form);
+    const info = {};
+    for (const [key, value] of forminfo) {
+        info[key] = value;
     }
-    return data;
-  }
-
+    return info;
+}
 async function validate(e) {
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    if (fname == "") {
-        alert("Please enter a First Name")
-        formLogin.fname.focus()
-        return false
-    }
-    if (lname == "") {
-        alert("Please enter a Last Name")
-        formLogin.lname.focus()
-        return false
-    }
-    if (email == "") {
-        alert("Please enter a Email")
-        formLogin.email.focus()
-        return false
-    }
-    if (password == "") {
-        alert("Please enter a Password")
-        formLogin.password.focus()
-        return false
+    e.preventDefault();
+    const response = await fetch("../YalaPay-data/users.json");
+    const data = await response.json();
+    console.log(data);
+    const user = formToObject(e.target);
+    console.log(user);
+    const exists = data.filter((x) => x.firstName == user.firstName && x.lastName==user.lastName && x.email==user.email  && x.password==user.password);
+    console.log(exists);
+    if(exists.length == 1){
+        window.location = "../dashboard.html"
     }
 }
-
